a45747ce4343ed5b986d1d92b66f07d813e51502
Sandy Andryanto <sandyandryanto@gmail.com>
2024-09-05 08:35:56 +0700

    up

--- backend/Controllers/ArticleController.cs
+++ backend/Controllers/ArticleController.cs
@@ -59,5 +59,38 @@ namespace backend.Controllers
             return Ok(new { status = true, data = list, message = "ok" });
         }
 
+        [HttpGet("user")]
+        [Authorize]
+        public IActionResult User(int page = 1, int limit = 10, String orderBy = "Id", String OrderDir = "desc", String? Search = null)
+        {
+            var user = (User)this.HttpContext.Items["User"];
+            FilterDTO filter = new FilterDTO();
+            filter.Page = page;
+            filter.Limit = limit;
+            filter.OrderBy = orderBy;
+            filter.OrderDir = OrderDir;
+            filter.Search = Search;
+            var list = _service.GetByUser(user, filter)
+                .Select(x => new ArticleListDTO()
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Description = x.Description,
+                    Image = x.Image,
+                    Categories = String.IsNullOrWhiteSpace(x.Categories) ? x.Categories.Split(',').ToList() : new List<String>(),
+                    Tags = String.IsNullOrWhiteSpace(x.Tags) ? x.Tags.Split(',').ToList() : new List<String>(),
+                    CreatedAt = x.CreatedAt,
+                    UpdatedAt = x.UpdatedAt,
+                    User = new UserDetailDTO()
+                    {
+                        FirstName = x.User.FirstName,
+                        LastName = x.User.LastName,
+                        Gender = x.User.Gender,
+                        Image = x.User.Image
+                    }
+                });
+            return Ok(new { status = true, data = list, message = "ok" });
+        }
+
     }
 }
